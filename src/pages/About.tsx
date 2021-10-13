5fa84e9067f5d6c7cae35d5c223648c607d4f753
Trevor Brown <trevorbrown.dev@gmail.com>
2021-10-13 19:29:28 -0400

    added firebase

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 export const About: React.FC = () => {
     return (
         <main className="hero-section about">
+
             <h2 id="hello-there">Hello There!</h2>
             <p>
-                My name is Trevor Brown!
+                My name is Trevor Brown! I'm a software engineer, web developer and a tutor!
             </p>
-        </main>
+
+        </main >
     );
 }
