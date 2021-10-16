bf7ba7899598b627c54639c6f6d335094f20c13d
Trevor Brown <trevorbrown.dev@gmail.com>
2021-10-15 23:22:29 -0400

    Added a bio, noodled with firebase functions and failed

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,7 +1,14 @@
+import { ButtonPrimary } from "./ButtonPrimary";
+
 export const ContactForm: React.FC = () => {
     return (
-        <form action="POST" className="contact-form">
+        <form action="POST" className="contact-form" onSubmit={(e) => {
+            e.preventDefault();
+            console.log("Sending Message...", e);
+        }}>
 
+
+            <ButtonPrimary type='submit'>Send!</ButtonPrimary>
         </form>
     );
 }
