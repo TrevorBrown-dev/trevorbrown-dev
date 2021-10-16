514c33090d9f81ec0e0ee124869dfe85d2b8769b
Trevor Brown <trevorbrown.dev@gmail.com>
2021-10-15 22:23:55 -0400

    added .env

--- functions/mail.ts
+++ functions/mail.ts
@@ -1,5 +1,6 @@
 import admin from 'firebase-admin';
 import functions from 'firebase-functions';
+import nodemailer from 'nodemailer';
 admin.initializeApp();
 interface ContactInfo {
     email: string;
@@ -11,4 +12,13 @@ export const emailContactInfo = functions.firestore.document('/contact/{document
     //Get the message
     const { email, firstName, lastName, message } = snap.data() as ContactInfo;
     //email me the message
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+        },
+    });
 });
