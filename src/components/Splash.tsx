e4c5e500939ae6aa911030457efd0cba02597583
Trevor Brown <trevorbrown.dev@gmail.com>
2021-10-17 15:00:29 -0400

    fixing some mobile issues

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -8,38 +8,37 @@ import { Star } from "./svg/splash/Star";
 export const Splash: React.FC = () => {
     return (
         <div className="splash" role="img">
-            <RingedPlanet style={{
+            <RingedPlanet className="planet" style={{
                 position: 'absolute', left: '1em', top: '-10em', transform: 'scale(0.5)'
             }} />
-            <PinkPlanet style={{
+            <PinkPlanet className="planet" style={{
                 position: 'absolute', left: '27em', top: '-10em', transform: 'scale(0.65)'
             }} />
-            <BluePlanet style={{
+            <BluePlanet className="planet" style={{
                 position: 'absolute', right: '35em', top: '-8em', transform: 'scale(0.6)'
             }} />
-            <Earth style={{
+            <Earth className="planet" style={{
                 position: 'absolute', right: '10em', top: '-5em', transform: 'scale(0.6)'
             }} />
-            <Star style={{
+            <Star className="planet" style={{
                 position: 'absolute', right: '10em', top: '-5em', transform: 'scale(0.6)'
             }} />
-            <Star style={{
+            <Star className="planet" style={{
                 position: 'absolute', left: '20em', top: '-5em', transform: 'scale(1) rotate(20deg)'
             }} />
-            <Star style={{
+            <Star className="planet" style={{
                 position: 'absolute', left: '50em', top: '-9em', transform: 'scale(0.5) rotate(50deg)'
             }} />
-            <Star style={{
+            <Star className="planet" style={{
                 position: 'absolute', right: '50em', top: '-5em', transform: 'scale(0.5) rotate(80deg)'
             }} />
-            <Star style={{
+            <Star className="planet" style={{
                 position: 'absolute', right: '30em', top: '-11em', transform: 'scale(0.5) rotate(80deg)'
             }} />
-            <Star style={{
+            <Star className="planet" style={{
                 position: 'absolute', left: '40em', top: '-4em', transform: 'scale(0.7) rotate(42deg)'
             }} />
             <div className="avatar">
-
                 < Avatar />
             </div>
         </div >
