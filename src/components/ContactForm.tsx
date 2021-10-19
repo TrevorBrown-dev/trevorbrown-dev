5e1256210f6b12f31d030cde99423a3a346c6d17
Trevor Brown <trevorbrown.dev@gmail.com>
2021-10-19 16:51:58 -0400

    Added feedback for form submission

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import { ContactInfo, sendContactInfo } from "../pages/Contact";
 import { ButtonPrimary } from "./ButtonPrimary";
 
 export const ContactForm: React.FC = () => {
+    const [submitted, setSubmitted] = useState(false);
     const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault();
         //Gather the Form Data
         const formData = new FormData(e.currentTarget);
         const data: { [key: string]: any } = {};
         formData.forEach((val, key) => data[key] = val)
-
+        setSubmitted(true);
+        setTimeout(() => {
+            setSubmitted(false);
+        }, 5000)
         //Clear The Form
         e.currentTarget.reset();
 
@@ -17,13 +22,21 @@ export const ContactForm: React.FC = () => {
     }
 
     return (
-        <form action="POST" className="contact-form" onSubmit={handleSubmit}>
+        <>
+            <form action="POST" className="contact-form" onSubmit={handleSubmit}>
 
-            <input type="text" id='firstName' name="firstName" placeholder="First Name" required />
-            <input type="text" id='lastName' name="lastName" placeholder="Last Name" required />
-            <input type="email" id='email' name="email" placeholder="Email" style={{ gridColumn: '1/-1' }} required />
-            <textarea name="message" id="message" placeholder="Message" style={{ gridColumn: '1/-1' }} required></textarea>
-            <ButtonPrimary type='submit' style={{ gridColumn: '1/-1' }}>Send!</ButtonPrimary>
-        </form>
+                <input type="text" id='firstName' name="firstName" placeholder="First Name" required />
+                <input type="text" id='lastName' name="lastName" placeholder="Last Name" required />
+                <input type="email" id='email' name="email" placeholder="Email" style={{ gridColumn: '1/-1' }} required />
+                <textarea name="message" id="message" placeholder="Message" style={{ gridColumn: '1/-1' }} required></textarea>
+                <ButtonPrimary type='submit' style={{ gridColumn: '1/-1' }}>Send!</ButtonPrimary>
+            </form>
+            {submitted &&
+                <div className="thank-you center-text" style={{ fontSize: '1.5em', marginTop: '1em' }}>
+                    Thanks for the message! I'll get back to you soon!<br />
+                    &ndash; Trevor
+                </div>
+            }
+        </>
     );
 }
